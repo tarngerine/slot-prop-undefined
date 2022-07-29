bde0905da6bb515a81774d5f5d94000f1bfafc6d
Julius Tarng <1629901+tarngerine@users.noreply.github.com>
2022-07-28 20:53:40 -0500

    init

---
+++ src/routes/Feed.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let items: any[];
+</script>
+
+<!-- 500 happens when we iterate over items and pass item as a slot prop-->
+<!--  && there is another named slot -->
+<!-- HACKY FIX 1: only use named slots -->
+{#each items as item}
+	<slot {item} />
+{/each}
+
+<!-- HACKY FIX 2: only have one slot -->
+<slot name="footer" />
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Feed from './Feed.svelte';
+
+	let events = [1, 2, 3];
+</script>
+
+<Feed items={events} let:item>
+	<div>{item}</div>
+
+	<footer slot="footer">Footer</footer>
+</Feed>
